30f8ca7a1f6ba091ec5af9702fa3e26eafcae5c7
Nicbudd <niczippy77@gmail.com>
2022-07-18 01:59:15 -0400

    Basics are done.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,54 @@
 pub mod modules;
+use crate::modules::Style;
+
+enum CallType {
+    Bash,
+    Discord,
+}
+
+fn main_but_cooler(return_type: CallType) {
+    let mut getModules = [
+        (modules::timeAndDate(), 6),
+        (modules::wx(), 11),
+    ];
+
+    
+
+    //let mut return_string = String::new();
+
+    let mut return_string = getModules
+                            .into_iter()
+                            .map(|(x, y)| if x.verify(y) {
+                                x
+                            } else {
+                                modules::TextBit {
+                                    text: String::from('X').repeat(y),
+                                    len: y,
+                                    style: Style::White,
+                                }
+                            })
+                            .map(|x| match return_type {
+                                CallType::Bash => {
+                                    match x.style {
+                                        Style::White => x.text,
+                                        Style::Red => format!("\x1b[31m{}\x1b[0m", x.text),
+                                        Style::Green => format!("\x1b[32m{}\x1b[0m", x.text),
+                                        Style::Blue => format!("\x1b[33m{}\x1b[0m", x.text),
+                                        Style::Bold => format!("\x1b[1m{}\x1b[0m", x.text),
+                                        _ => x.text,
+                                    }
+                                },
+                                _ => x.text, 
+                            })
+                            .collect::<Vec<String>>()
+                            .join("-");
+
+    
+    
+
+    println!("{return_string}");
+}
 
 fn main() {
-    let getModules = [
-        (modules::timeAndDate(), 6),
-    ];
-    
-    let textFields = getModules
-                        .iter()
-                        .map(|(x, y)| x.verify(*y).unwrap());
-                        
-    
-    
-    let mut return_string = String::new();
-    
-
-    println!("\x1b[31mHello, world!\x1b[0m");
+    main_but_cooler(CallType::Bash);
 }
--- src/modules.rs
+++ src/modules.rs
@@ -1,12 +1,23 @@
 use chrono::{DateTime, Utc};
+use ureq;
 
-struct TextBit {
-    text: String,
-    len: usize, // should be a constant! do not determine it dynamically with text.len() or something.
-    style: Style,
+pub struct TextBit {
+    pub text: String,
+    pub len: usize, // should be a constant! do not determine it dynamically with text.len() or something.
+    pub style: Style,
 }
 
-enum Style {
+impl TextBit {
+    pub fn check_len(&self) -> bool {
+        self.text.len() == self.len
+    }
+
+    pub fn verify(&self, len: usize) -> bool {
+        self.text.len() == len
+    }
+}
+
+pub enum Style {
     White,
     Red,
     Green,
@@ -21,20 +32,6 @@ enum Style {
 }
 
 
-impl TextBit {
-    pub fn check_len(&self) -> bool {
-        self.text.len() == self.len
-    }
-
-    pub fn verify(&self, len: usize) -> Result<String, ()> {
-        match self.text.len() == len {
-            true => Ok(self.text),
-            false => Err(()),
-        }
-    }
-}
-
-
 // MODULES
 
 pub fn timeAndDate() -> TextBit {
@@ -44,3 +41,20 @@ pub fn timeAndDate() -> TextBit {
     TextBit {text: format, len: 6, style: Style::Bold}
 }
 
+fn getWx(station: &str, network: &str) -> Result<String, ()> {
+    let url = &format!("http://mesonet.agron.iastate.edu/json/current.py?station={station}&network={network}");
+
+    ureq::get(url)
+        .call().map_err(|_| ())?
+        .into_string().map_err(|_| ())
+}
+
+pub fn wx() -> TextBit {
+    match getWx("PSM", "NH_ASOS") {
+        Ok(s) => {
+            println!("{}", s);
+            TextBit{text: s, len: 11, style: Style::Red} 
+        },
+        Err(_) => TextBit{text: String::from("UNAVAILABLE"), len: 11, style: Style::Red},
+    }
+}
